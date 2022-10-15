bd55eaf1614e2a39629c40ee348e16c9af872dc1
Vitok70 <vitokunev70@gmail.com>
2022-10-15 12:29:28 +0300

    feat: task extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,49 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
+function repeater(str, options) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+let arrResult =[];
+let strResult ='';
+str = String(str); 
+options.addition = String(options.addition); 
+ 
+if (options.separator === undefined) {options.separator = '+'};
+if (options.additionSeparator === undefined) {options.additionSeparator = '|'};
+if (options.repeatTimes === undefined) {options.repeatTimes = 1};
+if (options.additionRepeatTimes === undefined) {options.additionRepeatTimes = 1};
+if (options.addition === 'undefined') {options.addition = ''};
+
+if (options.repeatTimes == 0){
+  if(options.additionRepeatTimes == 0){arrResult.push(str,options.addition); }
+   else {  arrResult.push(str);
+           for (let i=0; i < options.additionRepeatTimes; i++ ){
+             arrResult.push(options.addition,options.additionSeparator);
+          }
+       }
+}
+else { if(options.additionRepeatTimes == 0){
+        for (let i=0; i < options.repeatTimes; i++ ){
+         arrResult.push(str,options.separator);}
+       arrResult.pop(); 
+       }
+       else {
+           for (let i=0; i < options.repeatTimes; i++ ){
+             arrResult.push(str);
+             for (let j=0; j < options.additionRepeatTimes; j++ ){
+                 arrResult.push(options.addition,options.additionSeparator);
+             }
+           arrResult.pop();
+           arrResult.push(options.separator);
+           }
+           arrResult.pop();
+       }
+   }     
+
+   strResult = arrResult.join('');
+   return strResult;
+  
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -11,7 +11,6 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
  */
 function transform(arr) {
   // throw new NotImplementedError('Not implemented');
@@ -19,29 +18,22 @@ function transform(arr) {
   let sequences = ['--discard-next', '--discard-prev','--double-next','--double-prev'];
   let transformed = [];  
   if (!Array.isArray(arr)) {throw new Error ("\'arr\' parameter must be an instance of the Array!");}
+
   for (let i=0; i < arr.length; i++){
-      if (!sequences.includes(arr[i],0)) {transformed.push(arr[i]); }
+      if (!sequences.includes(arr[i],0)) { transformed.push(arr[i]); }
       else 
          switch(arr[i]){
-              case '--discard-next':  {if (i != arr.length-1) {i++; break;}
-                                        else break;}
-              case '--discard-prev':  {if (i != 0) {transformed.splice(i-1,1); break;}
-                                        else break;}
-              case  '--double-next':  {if (i != arr.length-1) {transformed.push(arr[i+1]); break;}
-                                        else break; }
-              case  '--double-prev':  {if (i != 0) {
-                                          if (arr[i-2] != '--discard-next') {
-                                            transformed.push(arr[i-1]); break;
-                                          } 
-                                          else break;
-                                        }
-                                        else break;
-                                      }
+           case '--discard-next':  {if (i != arr.length-1) {i++; break;} else break;}
+           case '--discard-prev':  {if (i != 0) {transformed.splice(i-1,1); break;} else break;}
+           case  '--double-next':  {if (i != arr.length-1) {transformed.push(arr[i+1]); break;} else break; }
+           case  '--double-prev':  {if (i != 0) {
+                                       if (arr[i-2] != '--discard-next') { transformed.push(arr[i-1]); break;} 
+                                          else break;}
+                                    else break;
+                                    }
           }
   }
 return transformed;
-
-
 }
 
 module.exports = {
